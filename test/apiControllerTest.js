06c22676e27aebb86d4c6a630b5250d7b3bd45f1
Francesco Sacchi <sacchi@intre.it>
2017-02-12 21:27:17 +0100

    added tests for responseFactory and removed others

--- test/apiControllerTest.js
+++ test/apiControllerTest.js
@@ -5,49 +5,25 @@ const apiController = require('../controllers/apiController');
 
 describe('apiController', function(){
 
-    describe('getStatus', function(){
+    describe('responseFactory', function(){
+
         before(function(){
-            git.getStatus = function(callback){
-                callback(['test'])
-            };
             this.resMock = {
                 setHeader:function(...args){this.setHeaderArgs = args;},
                 send: function(arg){this.sendArgs = arg}
             };
+            this.generatedFunction = apiController.responseFactory((callback)=>callback(['test']));
         });
+        it('the generated function should set header content type application/json', function(){
 
-        it('should set header content type application/json', function(){
-            apiController.getStatus(null, this.resMock);
+            this.generatedFunction(null, this.resMock);
             assert(this.resMock.setHeaderArgs.length === 2);
             assert(this.resMock.setHeaderArgs[0] === 'Content-Type');
             assert(this.resMock.setHeaderArgs[1] === 'application/json');
         });
 
-        it('should send the json from the gitgetstatus call', function(){
-            apiController.getStatus(null, this.resMock);
-            assert(this.resMock.sendArgs==='["test"]');
-        });
-    });
-    describe('getTree', function(){
-        before(function(){
-            git.getTree = function(callback){
-                callback(['test'])
-            };
-            this.resMock = {
-                setHeader:function(...args){this.setHeaderArgs = args;},
-                send: function(arg){this.sendArgs = arg}
-            };
-        });
-
-        it('should set header content type application/json', function(){
-            apiController.getTree(null, this.resMock);
-            assert(this.resMock.setHeaderArgs.length === 2);
-            assert(this.resMock.setHeaderArgs[0] === 'Content-Type');
-            assert(this.resMock.setHeaderArgs[1] === 'application/json');
-        });
-
-        it('should send the json from the gitgetstatus call', function(){
-            apiController.getTree(null, this.resMock);
+        it('the generated function should send the json given to the callback', function(){
+            this.generatedFunction(null, this.resMock);
             assert(this.resMock.sendArgs==='["test"]');
         });
     });
