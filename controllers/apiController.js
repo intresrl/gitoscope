cb467c1e9e7e82dbf4cae69c8f0c16d12b38f032
Francesco Sacchi <sacchi@intre.it>
2017-02-19 21:19:20 +0100

    Started refactoring and using promise instead of callback

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -5,10 +5,18 @@ function respondWithJson(res, data) {
 }
 
 function responseFactory(gitFunction){
+    return function(req, res, next) {
+        res.setHeader('Content-Type', 'application/json');
+        gitFunction((data) => respondWithJson(res, data))
+    }
+}
+
+function promiseResponseFactory(gitFunction){
     return function(req, res, next){
         res.setHeader('Content-Type', 'application/json');
-        gitFunction((data)=>respondWithJson(res, data))
+        gitFunction().then((data)=>respondWithJson(res, data))
     }
+
 }
 
 function parametricResponseFactory(gitFunction){
@@ -18,10 +26,10 @@ function parametricResponseFactory(gitFunction){
     }
 }
 
-const getTree = responseFactory(git.getTree);
+const getTree = promiseResponseFactory(git.getTree);
 const getStatus = responseFactory(git.getStatus);
 const getEntry = parametricResponseFactory(git.getFile);
 const getDiff = parametricResponseFactory(git.getDiff);
 const getDiffCached = parametricResponseFactory(git.getDiffCached);
 
-module.exports = {responseFactory, parametricResponseFactory, getStatus, getTree, getEntry, getDiff, getDiffCached};
+module.exports = {responseFactory, parametricResponseFactory, getStatus, getTree, getEntry, getDiff, getDiffCached};[]
