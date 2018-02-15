2a04af967883c00768195cc3aa72fee02674a40b
Caglio Andrea <caglio@intre.it>
2018-02-15 12:33:04 +0100

    creati componenti react per titolo, messaggio di benvenuto e Header

--- public/javascripts/reactClient.js
+++ public/javascripts/reactClient.js
@@ -1,15 +1,62 @@
 //alert("qui ci metto tutto quello che serve per react")
 $( document ).ready(function() {
 
-ReactDOM.render(
-	React.createElement(
-		'h1',
-		null,
-		'GitLens'
-		),
-		document.getElementById('reactApp')
-	);
+	class Title extends React.Component {
+		render () {
+			return (
+				<h1>GitLens</h1>
+				)
+		}
+	}
 
+	class Message extends React.Component {
+		render () {
+			return this.props.children
+		}
+	}
+
+	class ReloadButton extends React.Component {
+		render () {
+			return (
+				<button onClick={loadData}>reload</button>
+				)
+		}
+	}
+
+	function loadData(){
+		updateStatus();
+		myLoadAreas();
+	}
+
+	class Header extends React.Component {
+		render () {
+			return (
+				<div className="header-entry">
+					<div className="area-title">Working copy</div>
+					<div className="area-title">Staging Area</div>
+					<div className="rea-title">HEAD commit</div>
+				</div>
+				)
+		}
+	}
+
+	ReactDOM.render(
+		<div>
+			<Title />
+			<Message>Welcome to GitLens</Message>
+		</div>,
+		document.getElementById('title')
+		);
+
+	ReactDOM.render(
+		<ReloadButton />,
+		document.getElementById('reload')
+		);
+
+	ReactDOM.render(
+		<Header />,
+		document.getElementById('header')
+		);
 
 }
 )
