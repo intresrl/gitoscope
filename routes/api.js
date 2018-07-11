a1d13a4d71344701c8e2ef839023fc6f0e114a6d
Francesco Sacchi <sacchi@intre.it>
2018-07-11 09:21:51 +0200

    Specify that some endpoints are used by internals page

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ router.get('/entry/:name([\\w.]+)', apiController.getEntry);
 router.get('/diff/:name([\\w.]+)', apiController.getDiff);
 router.get('/diffcached/:name([\\w.]+)', apiController.getDiffCached);
 
+// endpoints for internals
 router.get('/commits/:commitId(\\w+)', apiController.getCommit);
 router.get('/trees/:treeId(\\w+)', apiController.getTreeRest);
 router.get('/blobs/:blobId(\\w+)', apiController.getBlobRest);
